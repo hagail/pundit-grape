d2282a786a1da77b259b6952036f1c960f18f029
Hagai Levin <hagai.levin@klarna.com>
2015-03-02 16:36:32 +0200

    add authorize block to grape DSL

--- lib/pundit/grape.rb
+++ lib/pundit/grape.rb
@@ -1,7 +1,39 @@
 require "pundit/grape/version"
 
-module Pundit
-  module Grape
-    # Your code goes here...
+
+module Grape
+  module Pundit
+    module Authorization
+      extend ActiveSupport::Concern
+
+      module ClassMethods
+        def authorize(&block)
+          authorization = ->(_) {
+            authorized = instance_eval(&block)
+            unless authorized
+              fail Grape::Pundit::Exceptions::AuthorizationError
+            end
+          }
+
+          namespace_stackable(:after_validations, authorization)
+        end
+      end
+    end
+
+    module Exceptions
+      class AuthorizationError < Grape::Exceptions::Base
+        def initialize
+          super message: "API Authorization Failed.", status: 401
+        end
+      end
+    end
+  end
+
+  class API
+    include Grape::Pundit::Authorization
+  end
+
+  class Endpoint
+    include Pundit
   end
 end
